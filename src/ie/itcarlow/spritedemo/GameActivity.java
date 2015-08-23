45993d99c3c99cfeea78250cdfbe4afa0ed02f00
alycen <alsunnid@gmail.com>
2015-08-23 22:04:25 +0100

    Not the LimitedFPSEngine causing the problem

--- src/ie/itcarlow/spritedemo/GameActivity.java
+++ src/ie/itcarlow/spritedemo/GameActivity.java
@@ -2,6 +2,8 @@ package ie.itcarlow.spritedemo;
 
 import java.io.IOException;
 
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
@@ -19,24 +21,8 @@ import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
-public class GameActivity extends BaseGameActivity {/*implements IOnSceneTouchListener, IUpdateHandler{
-	private static final int CAMERA_WIDTH = 720;
-	private static final int CAMERA_HEIGHT = 480;
-
-	private BitmapTextureAtlas mTexture;
-	private BitmapTextureAtlas mBearTexture;
+public class GameActivity extends BaseGameActivity {
 	
-	private ITextureRegion mBearTextureRegion;
-	private Scene mScene;
-
-	private Sprite mBear;
-	
-	private ITiledTextureRegion mCountTextureRegion;
-	
-	Vector velocity;
-	
-	private AnimatedSprite count10Sprite; */
-
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		final Camera camera = new Camera(0, 0, 480, 800);
@@ -54,60 +40,25 @@ public class GameActivity extends BaseGameActivity {/*implements IOnSceneTouchLi
 
     private void loadGfx() {     
         BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");  
-        /*mTexture = new BitmapTextureAtlas(getTextureManager(), 320,32);  
-        mBearTexture = new BitmapTextureAtlas(getTextureManager(), 46, 54);
-        mCountTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(mTexture, this, "count10.png", 0, 0, 10, 1);
-        mBearTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBearTexture, this, "austrian_bear.png", 0,0);
-        		
-        mTexture.load();
-        mBearTexture.load();*/
+        
     }
 
     @Override
   	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
   			throws IOException {
-/*
-  		this.mScene = new Scene();
-  		this.mScene.setBackground(new Background(0, 125, 58));
-  		// Register this activity as a scene touch listener
-  		this.mScene.setOnSceneTouchListener(this);
-  	    pOnCreateSceneCallback.onCreateSceneFinished(this.mScene);  	*/	
+    	
   	}
 
     @Override
 	public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) 
           throws IOException {
 
-       // Setup coordinates for the sprite in order that it will
-       //  be centered on the camera.
-	 /* final float centerX = (CAMERA_WIDTH - this.mCountTextureRegion.getWidth()) / 2;
-	   final float centerY = (CAMERA_HEIGHT - this.mCountTextureRegion.getHeight()) / 2; 
-	   
-	   mBear = new Sprite(centerX, centerY, mBearTextureRegion,  this.mEngine.getVertexBufferObjectManager());
-	// Create a sprite and add it to the scene. mSpriteTextureRegion, this.mEngine.getVertexBufferObjectManager());
-	   //mSpriteTextureRegion, this.mEngine.getVertexBufferObjectManager());
-
-	   count10Sprite = new AnimatedSprite(centerX + 100, centerY, mCountTextureRegion, this.mEngine.getVertexBufferObjectManager()) {
-	     @Override
-	     public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
-	        final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-	              this.setPosition(pSceneTouchEvent.getX() -  
-	                     this.getWidth() / 2,   
-	                     pSceneTouchEvent.getY() - this.getHeight() / 2);
-	              return true;
-	     }
-	   };
-	   this.mScene.registerTouchArea(count10Sprite);
-	   
-	   count10Sprite.animate(1000);
-	   
-	   mScene.attachChild(count10Sprite);
-	   mScene.attachChild(mBear);
-	   // register this activity as an update handler with the engine
-	   this.mEngine.registerUpdateHandler(this);
-	   pOnPopulateSceneCallback.onPopulateSceneFinished(); */
     }
      
+    @Override 
+    public Engine onCreateEngine(EngineOptions pEngineOptions) {
+    	return new LimitedFPSEngine(pEngineOptions, 60);
+    }
     
 	/*@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
